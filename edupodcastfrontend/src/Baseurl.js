66d16ddc9b6f5d4b888648704f165502971d3228
ajeena <ajeena.sics@gmail.com>
2024-05-24 17:44:27 +0530

    podcast initiated

---
+++ edupodcastfrontend/src/Baseurl.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+
+  baseURL: 'http://localhost:4008/educational_podcasting_api', 
+  // baseURL: 'http://hybrid.srishticampus.in/educational_podcasting_api/', 
+
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosInstance
